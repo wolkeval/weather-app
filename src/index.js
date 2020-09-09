20fe05ad84d69bef5bea4d67685725ccbea33a61
wolkeval <68792665+wolkeval@users.noreply.github.com>
2020-09-09 23:15:56 +0200

    Added current date and time

--- src/index.js
+++ src/index.js
@@ -4,49 +4,22 @@ let celsiusFlag = true;
 let apiKey = "e04e51dd1592166f33d5c79d198d4731";
 let units = "metric";
 
-// Receives the API response either from retrieveByCoordinates or retrieveByCity and displays the weather
-function showWeather(response) {
-  let currentTemp = document.querySelector("#current-degrees");
-  let currentCity = document.querySelector("#current-city");
-  let humidity = document.querySelector("#humidity");
-  let wind = document.querySelector("#wind");
-  let description = document.querySelector(".description");
-  currentTemp.innerHTML = Math.round(response.data.main.temp);
-  currentCity.innerHTML = response.data.name;
-  humidity.innerHTML = `${response.data.main.humidity}%`;
-  wind.innerHTML = `${Math.round(response.data.wind.speed)} km/h`;
-  description.innerHTML = response.data.weather[0].description;
+// DATE & TIME HANDLER // =====================================================
+
+// Given target offset wrt UTC (in sec), returns target timestamp (in ms),
+// where target is a specific city
+function getTargetTimestamp(targetOffsetInSec) {
+  let now = new Date();
+  // now.getTimezoneOffset() returns local offset wrt UTC in minutes as UTC time - your local time
+  let localOffsetInMs = now.getTimezoneOffset() * 60 * 1000;
+  let targetOffsetInMs = targetOffsetInSec * 1000;
+  let targetTimestamp = now.getTime() + localOffsetInMs + targetOffsetInMs;
+  return targetTimestamp;
 }
 
-function retrieveByCoordinates(position) {
-  let latitude = position.coords.latitude;
-  let longitude = position.coords.longitude;
-  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=${units}`;
-  axios.get(apiUrl).then(showWeather);
-}
+function formatDate(timestamp) {
+  let now = new Date(timestamp);
 
-// Receives a city either on load (Tokyo) or from handleForm, makes an API call and calls showWeather
-function retrieveByCity(city) {
-  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=${units}`;
-  axios.get(apiUrl).then(showWeather);
-}
-
-// Displays the weather for Tokyo on load
-retrieveByCity("Tokyo");
-
-function handleForm(city) {
-  city.preventDefault();
-  let cityInput = document.querySelector("#city-input");
-  retrieveByCity(cityInput.value);
-}
-
-let form = document.querySelector("form");
-form.addEventListener("submit", handleForm);
-
-// =======================================================================
-
-// DATE HANDLER ==========================================================
-function formatDate(date) {
   let days = [
     "Sunday",
     "Monday",
@@ -54,7 +27,7 @@ function formatDate(date) {
     "Wednesday",
     "Thursday",
     "Friday",
-    "Saturday"
+    "Saturday",
   ];
 
   let months = [
@@ -69,21 +42,16 @@ function formatDate(date) {
     "September",
     "October",
     "November",
-    "December"
+    "December",
   ];
 
-  return `${days[now.getDay()]}, ${now.getDate()} ${
-    months[now.getMonth()]
-  } ${now.getFullYear()}`;
+  let weekday = days[now.getDay()];
+  let day = now.getDate();
+  let month = months[now.getMonth()];
+  let year = now.getFullYear();
+  let fullDate = `${weekday}, ${day} ${month} ${year}`;
+  return fullDate;
 }
-
-let now = new Date();
-let currentDate = document.querySelector("#date");
-currentDate.innerHTML = formatDate(now);
-// =======================================================================
-
-// TIME ==================================================================
-let currentTime = document.querySelector("#time");
 
 // Displays single-digit numbers with leading zero
 function leadingZero(value) {
@@ -94,27 +62,62 @@ function leadingZero(value) {
   }
 }
 
-let currentHour = now.getHours();
-let currentMinutes = now.getMinutes();
-currentTime.innerHTML = `${leadingZero(currentHour)}:${leadingZero(
-  currentMinutes
-)}`;
+function formatTime(timestamp) {
+  let now = new Date(timestamp);
+  console.log(now);
+  let hours = now.getHours();
+  let minutes = now.getMinutes();
+  let time = `${leadingZero(hours)}:${leadingZero(minutes)}`;
+  return time;
+}
 
-// ALTERNATIVE METHOD
-// Displays single-digit hours with leading zero
-// let currentHour = now.getHours().toString();
-// if (currentHour.length === 1) {
-//   currentHour = `0${currentHour}`;
-// }
-
-// // Displays single-digit minutes with leading zero
-// let currentMinutes = now.getMinutes().toString();
-// if (currentMinutes.length === 1) {
-//   currentMinutes = `0${currentMinutes}`;
-// }
-
-// currentTime.innerHTML = `${currentHour}:${currentMinutes}`;
 // =======================================================================
+
+// Receives the API response either from retrieveByCoordinates or retrieveByCity and displays the weather
+function showWeather(response) {
+  let temperatureElement = document.querySelector("#current-degrees");
+  let cityElement = document.querySelector("#current-city");
+  let dateElement = document.querySelector("#current-date");
+  let timeElement = document.querySelector("#current-time");
+  let humidityElement = document.querySelector("#humidity");
+  let windElement = document.querySelector("#wind");
+  let descriptionElement = document.querySelector("#description");
+
+  temperatureElement.innerHTML = Math.round(response.data.main.temp);
+  cityElement.innerHTML = response.data.name;
+  humidityElement.innerHTML = `${response.data.main.humidity}%`;
+  windElement.innerHTML = `${Math.round(response.data.wind.speed)} km/h`;
+  descriptionElement.innerHTML = response.data.weather[0].description;
+  let timestamp = getTargetTimestamp(response.data.timezone);
+  dateElement.innerHTML = formatDate(timestamp);
+  timeElement.innerHTML = formatTime(timestamp);
+}
+
+function retrieveByCoordinates(position) {
+  let latitude = position.coords.latitude;
+  let longitude = position.coords.longitude;
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=${units}`;
+  axios.get(apiUrl).then(showWeather);
+}
+
+// Receives a city either on load (Tokyo) or from handleForm, makes an API call and calls showWeather
+function retrieveByCity(city) {
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=${units}`;
+  console.log(apiUrl);
+  axios.get(apiUrl).then(showWeather);
+}
+
+// Displays the weather for Tokyo on load
+retrieveByCity("Tokyo");
+
+function handleForm(city) {
+  city.preventDefault();
+  let cityInput = document.querySelector("#city-input");
+  retrieveByCity(cityInput.value);
+}
+
+let form = document.querySelector("form");
+form.addEventListener("submit", handleForm);
 
 // SWITCH BUTTON // ======================================================
 function toFahrenheit(celsius) {
